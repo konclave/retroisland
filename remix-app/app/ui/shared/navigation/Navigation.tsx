ba0e84a90cd8544a2698d9857a37ba4b6b523bdf
Ivan Vasilyev <vianomore@gmail.com>
2022-09-15 21:50:52 +0200

    chore: extract navigation icons

--- remix-app/app/ui/shared/navigation/Navigation.tsx
+++ remix-app/app/ui/shared/navigation/Navigation.tsx
@@ -4,6 +4,8 @@ import cx from 'classnames';
 
 import styles from './navigation.css';
 import stylesDesktop from './navigation.d.css';
+import { IconBurger } from '../icons/icon-burger';
+import { IconCross } from '../icons/icon-cross';
 
 export const links = () => [
   { rel: 'stylesheet', href: styles },
@@ -23,49 +25,32 @@ export const Navigation = () => {
 
   useEffect(() => {
     if (isVisible) {
-      document.body.classList.add('no-scroll')
+      document.body.classList.add('no-scroll');
     } else {
-      document.body.classList.remove('no-scroll')
+      document.body.classList.remove('no-scroll');
     }
-    return () => document.body.classList.remove('no-scroll')
-  }, [isVisible])
+    return () => document.body.classList.remove('no-scroll');
+  }, [isVisible]);
 
   return (
-    <section className={cx({ navigation: true, navigation_visible: isVisible })}>
+    <section
+      className={cx({ navigation: true, navigation_visible: isVisible })}
+    >
       <button
         className="navigation-button navigation-open"
         type="button"
         onClick={toggleNavigation}
       >
-        <svg
-          className="navigation-open__icon"
-          width="24"
-          height="24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect x="4" y="5" width="16" height="2" rx="1" fill="#000" />
-          <rect x="4" y="11" width="16" height="2" rx="1" fill="#000" />
-          <rect x="4" y="17" width="16" height="2" rx="1" fill="#000" />
-        </svg>
+        <IconBurger className="navigation-open__icon" />
         Меню
       </button>
       <nav className="main-navigation-wrap">
-        <button className="navigation-button navigation-close" type="button" onClick={toggleNavigation}>
-          <svg
-            className="navigation-close__icon"
-            width="16"
-            height="16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1 1l14 14m0-14L1 15"
-              stroke="#222"
-              strokeWidth="2"
-              strokeLinecap="round"
-            />
-          </svg>
+        <button
+          className="navigation-button navigation-close"
+          type="button"
+          onClick={toggleNavigation}
+        >
+          <IconCross className="navigation-close__icon" />
         </button>
         <ul className="main-navigation">
           <li className="main-navigation__item">
