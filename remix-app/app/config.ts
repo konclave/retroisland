e4a8024a2208086f8496ad2bab5aa157029d6050
Ivan Vasilyev <vianomore@gmail.com>
2022-09-17 22:08:23 +0200

    fix: process env on client side

--- remix-app/app/config.ts
+++ remix-app/app/config.ts
@@ -1,1 +1,6 @@
 export const BREAKPOINT_DESKTOP = '800px';
+
+const spaceId = typeof process !== 'undefined' ? process.env['CONTENTFUL_SPACE_ID'] : '';
+const environment = typeof process !== 'undefined' ? process?.env['CONTENTFUL_ENVIRONMENT'] : '';
+const accessToken = typeof process !== 'undefined' ? process?.env['CONTENTFUL_ACCESS_TOKEN'] : '';
+export const CONTENTFUL_ENTRIES_URL = `https://cdn.contentful.com/spaces/${spaceId}/environments/${environment}/entries?access_token=${accessToken}`;
