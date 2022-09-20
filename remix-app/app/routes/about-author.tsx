69273b8b3b0cfa7b288f55221c02eb814864b9eb
Ivan Vasilyev <vianomore@gmail.com>
2022-09-20 23:44:42 +0200

    feat: add blank about pages

---
+++ remix-app/app/routes/about-author.tsx
@@ -0,0 +1,5 @@
+export default function AboutAuthor() {
+  return (
+    <p>About author</p>
+  )
+}
---
+++ remix-app/app/routes/about-project.tsx
@@ -0,0 +1,5 @@
+export default function AboutProject() {
+  return (
+    <p>About project</p>
+  )
+}
