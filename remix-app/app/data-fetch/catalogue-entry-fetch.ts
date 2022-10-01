30aec9715485898bd870cfe831a31e58ee7dce6f
Ivan Vasilyev <vianomore@gmail.com>
2022-10-01 22:53:47 +0200

    feat: support 404 in catalogue items

--- remix-app/app/data-fetch/catalogue-entry-fetch.ts
+++ remix-app/app/data-fetch/catalogue-entry-fetch.ts
@@ -5,12 +5,17 @@ import type { CatalogueEntryDto } from './catalogue-list-fetch';
 
 export async function fetchCatalogueEntry(
   slug: string
-): Promise<CatalogueEntryDto> {
+): Promise<CatalogueEntryDto | null> {
   const response = await client.getEntries<ICatalogueEntryFields>({
     content_type: 'catalogueEntry',
     'fields.slug': slug,
     select: 'sys.id,fields',
     include: 2,
   });
+
+  if (response.items.length === 0) {
+    return null;
+  }
+
   return mapToDto(response.items[0]);
 }
