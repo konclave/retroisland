457a8378addf5682924749c87ad38f5fa79488fb
Ivan Vasilyev <vianomore@gmail.com>
2022-10-01 20:40:14 +0200

    fix: jouele initialisation on routes change

--- remix-app/app/ui/shared/track/track-audio.tsx
+++ remix-app/app/ui/shared/track/track-audio.tsx
@@ -1,10 +1,18 @@
+import { useEffect, useRef } from 'react';
 import type { RequestedTrackItemDto } from '~/data-fetch';
 
 export const TrackAudio = ({ item }: { item: RequestedTrackItemDto }) => {
+  const ref = useRef<HTMLAnchorElement | null>(null);
+  useEffect(() => {
+    if (typeof document !== 'undefined' && window.jQuery && ref.current) {
+      window.jQuery(ref.current).jouele();
+    }
+  }, [ref.current]);
   return (
     <>
       <div className="track-wrap">
         <a
+          ref={ref}
           className="jouele"
           data-hide-timeline-on-pause="true"
           id={item.id}
