7adfe4b197545225df9c75cf517edb7e2267b9a7
Ivan Vasilyev <vianomore@gmail.com>
2022-10-02 14:18:55 +0200

    feat: add 404

--- remix-app/app/root.tsx
+++ remix-app/app/root.tsx
@@ -6,6 +6,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
 } from '@remix-run/react';
 
 import normalizeStyles from '~/styles/normalize.css';
@@ -13,8 +14,10 @@ import colorsStyles from '~/styles/colors.css';
 import fontsStyles from '~/styles/fonts.css';
 import mainStyles from '~/styles/main.css';
 import desktopStyles from '~/styles/desktop.css';
+
 import { Navigation, links as navitationLinks } from '~/ui/shared/navigation';
 import { About, links as aboutLinks } from '~/ui/shared/about';
+import { ErrorPage, links as errorPageLinks } from '~/ui/error-page';
 import { BREAKPOINT_DESKTOP } from '~/config';
 
 export const links = () => [
@@ -33,6 +36,7 @@ export const links = () => [
   },
   ...navitationLinks(),
   ...aboutLinks(),
+  ...errorPageLinks(),
 ];
 
 export const meta: MetaFunction = () => ({
@@ -55,6 +59,23 @@ export default function App() {
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  return (
+    <html lang="ru-Ru">
+      <head>
+        <title>Oops!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <ErrorPage error={caught} />
+        <Scripts />
       </body>
     </html>
   );
