b174b5b0bf94a40539884215a041af85b3335b0d
Ivan Vasilyev <vianomore@gmail.com>
2022-10-13 20:27:07 +0200

    chore: change spinner

--- remix-app/app/ui/shared/spinner/spinner.tsx
+++ remix-app/app/ui/shared/spinner/spinner.tsx
@@ -6,7 +6,10 @@ export const links = getLinks(styles);
 export const Spinner = () => {
   return (
     <div className="spinner-box">
-      <div className="spinner"></div>
+      <div className="spinner">
+        <div className="dot1"></div>
+        <div className="dot2"></div>
+      </div>
     </div>
   );
 };
