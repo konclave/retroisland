a12edad08c28a9b5530dd3dca4978811f604cdd2
Ivan Vasilyev <vianomore@gmail.com>
2022-10-17 23:10:26 +0200

    feat: add page content to index

--- remix-app/app/loaders/index-loader.ts
+++ remix-app/app/loaders/index-loader.ts
@@ -1,20 +1,25 @@
 import { fetchNews, fetchRequested } from '~/data-fetch';
+import { client } from '~/data-fetch/contentful-client';
+
 import type { LoaderFunction } from '@remix-run/node';
 import type { NewsItemDto, RequestedItemDto } from '~/data-fetch';
 
 export interface IndexLoaderData {
   news: NewsItemDto[];
   requested: RequestedItemDto[];
+  pageContent: any;
 }
 
 export const indexLoader: LoaderFunction = async () => {
-  const [news, requested] = await Promise.all([
+  const [news, requested, content] = await Promise.all([
     await fetchNews(),
     await fetchRequested(),
+    await client.getEntry('5JMor9jC2OuNal1Eb5kfhy'),
   ]);
 
   return {
     news: news.items,
     requested,
+    content,
   };
 };
