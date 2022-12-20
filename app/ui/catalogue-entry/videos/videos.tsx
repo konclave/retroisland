f9ec78d2a1176af4cb60bae097e2b0f6337726a2
Ivan Vasilyev <vianomore@gmail.com>
2022-12-20 22:51:40 +0100

    feat: video list

--- app/ui/catalogue-entry/videos/videos.tsx
+++ app/ui/catalogue-entry/videos/videos.tsx
@@ -1,27 +1,33 @@
 import { getLinks } from '~/utils';
-import { Video } from './video';
+import { Video, links as videoLinks } from './video';
+
+import type { VideoDto } from '~/data-fetch';
+
 import styles from './videos.css';
 import desktopStyles from './videos.d.css';
 
 const localLinks = getLinks(styles, desktopStyles);
-export const links = () => [...localLinks()];
+export const links = () => [...videoLinks(), ...localLinks()];
 
 interface VideosProps {
-  entries?: string[];
+  entries?: VideoDto[];
 }
 
 export const Videos = ({ entries }: VideosProps) => {
-  console.log(entries);
   if (!entries?.length) {
     return null;
   }
 
   return (
-    <article className="catalogue-item-videos content-section">
+    <article className="catalogue-item-videos">
       <h2 className="catalogue-item-videos__title section-title">Видео</h2>
-      {entries.map((video) => (
-        <Video video={video} key={video.url} />
-      ))}
+      <ul className="videos-list">
+        {entries.map((video) => (
+          <li className="videos-list__item" key={video.url}>
+            <Video video={video} />
+          </li>
+        ))}
+      </ul>
     </article>
   );
 };
