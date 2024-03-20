9b3a3565e6164ce82511336b8d4b51634f39696c
Ivan Vasilev <vianomore@gmail.com>
2024-03-20 13:07:58 +0100

    feat: news rss feed

---
+++ app/loaders/rss-loader.ts
@@ -0,0 +1,21 @@
+import { fetchNews } from '~/data-fetch';
+import { marked } from 'marked';
+
+export type RssEntry = {
+  title: string;
+  link: string;
+  description: string;
+  pubDate: string;
+  author?: string;
+  guid?: string;
+};
+
+export const rssLoader = async () => {
+  const news = await fetchNews();
+  return news.items.map((item) => ({
+    title: marked.parse(item.text),
+    link: 'https://retroisland.net/news-archive',
+    description: item.text,
+    pubDate: new Date(item.date).toUTCString(),
+  })) as RssEntry[];
+};
